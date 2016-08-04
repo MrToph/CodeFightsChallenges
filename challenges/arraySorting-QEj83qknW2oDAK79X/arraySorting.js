539af1e1c48e0559308289516bf4ec32debf7385
cmichel <christoph.michel.1992@gmail.com>
2016-08-04 23:06:38 +0200

    fixed formatting

--- challenges/arraySorting-QEj83qknW2oDAK79X/arraySorting.js
+++ challenges/arraySorting-QEj83qknW2oDAK79X/arraySorting.js
@@ -1,29 +1,29 @@
 arraySorting = x => {
-    S = 0
-    M = Math.max
-    for(i = 0; i < x.length; i++){
-        if(x[i] != i+1){
-            S = M(S, 1);
-            if(x[x[i]-1] != i+1){
-                S = M(S, 2);
-            } 
-        } 
+  S = 0;
+  M = Math.max;
+  for (i = 0; i < x.length; i++) {
+    if (x[i] != i + 1) {
+      S = M(S, 1);
+      if (x[x[i] - 1] != i + 1) {
+        S = M(S, 2);
+      }
     }
-    return S;
-}
+  }
+  return S;
+};
 
 // need 0 S if array sorted
 // need 1 step if swapping i with j makes both i and j end up in correct position
 // need 2 otherwise
 // function arraySorting(x) {
-//     var steps = 0;
+//     var steps = 0
 //     for(var i = 0, j = 1; i < x.length; i++, j++){
 //         if(x[i] != j){
-//             steps = Math.max(steps, 1);
+//             steps = Math.max(steps, 1)
 //             if(x[x[i]-1] != i+1){
-//                 steps = Math.max(steps, 2);
+//                 steps = Math.max(steps, 2)
 //             } 
 //         } 
 //     }
-//     return steps;
+//     return steps
 // }
